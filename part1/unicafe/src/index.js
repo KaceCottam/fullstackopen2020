dbb56d33c4f3f98fab85ae953e91315b158b7dac
KaceCottam <kace.cottam@wsu.edu>
2020-04-05 23:11:29 -0700

    exercise 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -10,6 +10,9 @@ const IncrementButton = ({text, state, stateFn}) => (
 )
 
 const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = good / all * 100
 
   return (
     <div>
@@ -17,6 +20,9 @@ const Statistics = ({good, neutral, bad}) => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
