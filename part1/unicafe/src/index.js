70ee966db3b6fe92b61b8f66a0938d75124d9fa3
KaceCottam <kace.cottam@wsu.edu>
2020-04-05 23:21:38 -0700

    Exercise 1.11

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -10,7 +10,7 @@ const IncrementButton = ({text, state, stateFn}) => (
 )
 
 const Statistics = ({good, neutral, bad}) => {
-  if([good, neutral, bad].reduce((x,y) => x + y) === 0) {
+  if(good + neutral + bad === 0) {
     return (
       <div>
         <h1>statistics</h1>
@@ -26,12 +26,30 @@ const Statistics = ({good, neutral, bad}) => {
   return (
     <div>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <tr>
+        <td>good</td>
+        <td>{good}</td>
+      </tr>
+      <tr>
+        <td>neutral</td>
+        <td>{neutral}</td>
+      </tr>
+      <tr>
+        <td>bad</td>
+        <td>{bad}</td>
+      </tr>
+      <tr>
+        <td>all</td>
+        <td>{all}</td>
+      </tr>
+      <tr>
+        <td>average</td>
+        <td>{average}</td>
+      </tr>
+      <tr>
+        <td>positive</td>
+        <td>{positive}%</td>
+      </tr>
     </div>
   )
 }
