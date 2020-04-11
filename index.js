2bfe20b8dc4c2cbcd296a2534605b7249dcb882b
KaceCottam <kace.cottam@wsu.edu>
2020-04-11 11:02:11 -0700

    Exercises 3.2 and 3.3

--- index.js
+++ index.js
@@ -33,6 +33,26 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+  const numberPeople = persons.length
+  const now = new Date()
+  res.send(`<p>Phonebook has info for ${numberPeople} people.</p>`
+         + `<p>${now}</p>`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    console.log(`GET /api/persons/${id} 200`)
+    res.json(person)
+  }
+  else {
+    console.log(`GET /api/persons/${id} 404`)
+    res.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
