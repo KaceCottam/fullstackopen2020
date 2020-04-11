8cd0bc60f836e8009dfbe6ad4722686c846f3f9e
KaceCottam <kace.cottam@wsu.edu>
2020-04-11 10:41:54 -0700

    Initial commit

--- part2/phonebook/src/components/notification.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const makeNotifyComponent = (className) => ({ message }) => {
-  if (message === null) { return null }
-
-  return (<div className={className}>{message}</div>)
-}
-
-export default makeNotifyComponent
