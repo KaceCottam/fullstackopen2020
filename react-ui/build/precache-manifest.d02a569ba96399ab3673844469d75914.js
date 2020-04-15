9715a99da4f17679d22fb58d0528e8c52f164e34
KaceCottam <kace.cottam@wsu.edu>
2020-04-14 19:12:28 -0700

    Exercise 3.10 pending, exercise 3.11 ready

---
+++ react-ui/build/precache-manifest.d02a569ba96399ab3673844469d75914.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ab396daac9fd0c277f7362c36efa8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbf53d7d6910e1a0cc6e",
+    "url": "/static/js/2.7e893735.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7e893735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b6dc16ea1479b2e6a21",
+    "url": "/static/js/main.2b961f5e.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
